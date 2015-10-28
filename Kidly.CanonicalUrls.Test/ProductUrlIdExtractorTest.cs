fb369c99f1a51424963477bbe361788d0be223be
phoenix\d.lepelley <phoenix\d.lepelley>
2015-10-28 13:40:51 +0000

    Extra test

--- Kidly.CanonicalUrls.Test/ProductUrlIdExtractorTest.cs
+++ Kidly.CanonicalUrls.Test/ProductUrlIdExtractorTest.cs
@@ -8,6 +8,7 @@ namespace Kidly.CanonicalUrls.Test
         [TestCase("/p/britax/britax-b-agile-3-wheel-pushchair/2/", 2)]
         [TestCase("/p/britax/teddybear/2", 2)]
         [TestCase("/p/somebrandname/someproductname/", null)]
+        [TestCase("/p/somebrandname/someproductname/notanumber", null)]
         [TestCase("/p/somebrandname/someproductname/4/?colour=red", 4)]
         public void Test(string url, int? expected)
         {
